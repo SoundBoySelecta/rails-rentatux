2cc8da8659044ff0c50759c67c36c5e979da192f
V.S <sndselecta@gmail.com>
2018-05-22 11:41:07 -0400

    added new fields plus 100 fake users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_210506) do
+ActiveRecord::Schema.define(version: 2018_05_22_152205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tuxedos", force: :cascade do |t|
+    t.string "color"
+    t.string "style"
+    t.string "condition"
+    t.string "size"
+    t.integer "year"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +39,11 @@ ActiveRecord::Schema.define(version: 2018_05_21_210506) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "city"
+    t.string "country"
+    t.integer "rating"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
