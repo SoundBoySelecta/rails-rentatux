212f987cd6117ff0c789897f523451205814c13a
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-05-22 11:33:30 -0400

    Tux Controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_210506) do
+ActiveRecord::Schema.define(version: 2018_05_22_135355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tuxedos", force: :cascade do |t|
+    t.string "color"
+    t.string "style"
+    t.string "condition"
+    t.string "size"
+    t.integer "year"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
