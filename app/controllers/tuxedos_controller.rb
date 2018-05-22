e15930ae512d5bab5f69e9143f259750a580ca7b
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-05-22 13:34:36 -0400

    Tux-Controller-Update

---
+++ app/controllers/tuxedos_controller.rb
@@ -0,0 +1,40 @@
+class TuxedosController < ApplicationController
+def index
+    @tuxedos = Tuxedo.all
+  end
+
+  def new
+    @tuxedo = Tuxedo.new
+  end
+
+  def show
+    @tuxedo = Tuxedo.find(params[:id])
+    @bookings = @tuxedo.bookings
+    @tuxedo = Tuxedo.new
+  end
+
+  def create
+    @tuxedo = Tuxedo.new(tux_params)
+    @tuxedo.user = @current_user
+    if @tuxedo.save
+      # Redirect It to Proper Route Here
+      # redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+
+  def destroy
+      @tuxedo = Tuxedo.find(params[:id])
+      @tuxedo.destroy
+      # Redirect It to Proper Route Here
+      # redirect_to cocktail_path(@dose.cocktail)
+  end
+end
+
+  private
+
+  def tux_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    params.require(:picture, :condition).permit(:style, :color, :size, :year)
+  end
+end
