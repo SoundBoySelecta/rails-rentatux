1f1c01b2128b6b2cb06b4e5c446301b30dc8fe3f
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-05-24 13:35:27 -0400

    Navbar Is Functional

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,41 @@
+class MembersController < ApplicationController
+  def dashboard
+    @my_tuxedos = current_user.tuxedos
+    @my_bookings = current_user.bookings
+    # @bookings_on_my_tuxedos = current_user.bookings_on_my_tuxedos
+  end
+
+  # has_many :bookings
+  # has_many :users
+
+  # def show
+  #   @member = Member.new
+  # end
+
+  # def edit
+  #   @members = Member.find(params[:id])
+  #   flash[:alert] = "You have successfully updated this booking"
+  #   redirect_to member_path(@member)
+  # end
+
+  # def update
+  #   @member.update(member_params)
+  #   flash[:alert] = "You have successfully updated this booking"
+  #   redirect_to member_path(@member)
+  # end
+
+  # def destroy
+  #   @member.destroy
+  #   redirect_to tuxedos_path
+  # end
+
+  # private
+
+  # def set_booking
+  #   @booking = Booking.find(params[:booking_id])
+  # end
+
+  # def set_tuxedo
+  #   @tuxedo = Tuxedo.find(params[:tuxedo_id])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
     resources :bookings
   end
 
+  get "dashboard", to: "members#dashboard", as: "dashboard"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
